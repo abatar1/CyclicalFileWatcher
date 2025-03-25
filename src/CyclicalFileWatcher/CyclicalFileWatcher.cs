1417deb767c5bfa1d66b46b9838c67db5df6cc59
abatar1 <evgenyhalzov@gmail.com>
2025-03-25 23:09:50 +0200

    refactoring

--- src/CyclicalFileWatcher/CyclicalFileWatcher.cs
+++ src/CyclicalFileWatcher/CyclicalFileWatcher.cs
@@ -11,32 +11,32 @@ namespace CyclicalFileWatcher;
 public sealed class CyclicalFileWatcher<TFileStateContent> : IAsyncDisposable
     where TFileStateContent : IFileStateContent
 {
-    private readonly IFileStateManager<TFileStateContent> _fileManager;
+    private readonly IFileStateManager<TFileStateContent> _fileStateManager;
     private readonly FileSubscriptionManager<TFileStateContent> _subscriptionManager;
     
     public CyclicalFileWatcher(IFileStateManagerConfiguration configuration)
     {
         _subscriptionManager = new FileSubscriptionManager<TFileStateContent>();
-        _fileManager = new FileStateManager<TFileStateContent>(configuration, _subscriptionManager);
+        _fileStateManager = new FileStateManager<TFileStateContent>(configuration, _subscriptionManager);
     }
 
     /// <exception cref="FileNotFoundException">file found on path filePath, ensure WatchAsync has been called</exception>
     public Task WatchAsync(FileWatcherParameters<TFileStateContent> parameters, CancellationToken cancellationToken)
     {
-        return _fileManager.WatchAsync(parameters, cancellationToken);
+        return _fileStateManager.WatchAsync(parameters, cancellationToken);
     }
     
     /// <exception cref="FileNotFoundException">file found on path filePath, ensure WatchAsync has been called</exception>
     /// <exception cref="KeyNotFoundException">file was found, but key is not</exception>
     public Task<IFileState<TFileStateContent>> GetAsync(string filePath, string fileKey, CancellationToken cancellationToken)
     {
-        return _fileManager.GetAsync(filePath, fileKey, cancellationToken);
+        return _fileStateManager.GetAsync(filePath, fileKey, cancellationToken);
     }
 
     /// <exception cref="FileNotFoundException">file found on path filePath, ensure WatchAsync has been called</exception>
     public Task<IFileState<TFileStateContent>> GetLatestAsync(string filePath, CancellationToken cancellationToken)
     {
-        return _fileManager.GetLatestAsync(filePath, cancellationToken);
+        return _fileStateManager.GetLatestAsync(filePath, cancellationToken);
     }
 
     public Task<FileSubscription> SubscribeAsync(string filePath, Func<IFileState<TFileStateContent>, Task> actionOnUpdate, CancellationToken cancellationToken)
@@ -51,7 +51,7 @@ public sealed class CyclicalFileWatcher<TFileStateContent> : IAsyncDisposable
     
     public async ValueTask DisposeAsync()
     {
-        await _fileManager.DisposeAsync();
+        await _fileStateManager.DisposeAsync();
         await _subscriptionManager.DisposeAsync();
     }
 }
