b5c875a58c7216ff4ebde778f66f1a6c82373c10
abatar1 <evgenyhalzov@gmail.com>
2025-04-27 04:14:08 +0300

    whops

---
+++ src/CyclicalFileWatcher/Base/FileStateContentFactory.cs
@@ -0,0 +1,5 @@
+﻿using System.Threading.Tasks;
+
+namespace FileWatcher.Base;
+
+public delegate Task<TFileStateContent> FileStateContentFactory<TFileStateContent>(string filePath) where TFileStateContent : IFileStateContent;
---
+++ src/CyclicalFileWatcher/Base/FileStateKeyFactory.cs
@@ -0,0 +1,5 @@
+﻿using System.Threading.Tasks;
+
+namespace FileWatcher.Base;
+
+public delegate Task<string> FileStateKeyFactory<in TFileStateContent>(string filePath, TFileStateContent fileStateContent) where TFileStateContent : IFileStateContent;
