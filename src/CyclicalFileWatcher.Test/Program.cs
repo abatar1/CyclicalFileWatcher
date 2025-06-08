36b8b06784231bb9ba199f1a60f9d0a18efa716f
abatar1 <evgenyhalzov@gmail.com>
2025-06-08 17:15:14 +0300

    Fixed example codee

--- src/CyclicalFileWatcher.Test/Program.cs
+++ src/CyclicalFileWatcher.Test/Program.cs
@@ -16,10 +16,10 @@ Console.CancelKeyPress += (_, e) =>
 var configuration = new FileStateManagerConfiguration
 {
     FileCheckInterval = TimeSpan.FromSeconds(fileCheckSecondsInterval),
-    ActionOnFileReloaded = id => Task.Run(() => Console.WriteLine("FileWatcher reload complete for file {0}", id.FilePath)),
-    ActionOnFileReloadFailed = ex => Task.Run(() => Console.WriteLine("FileWatcher reload failure for file {0}", ex.FileIdentifier.FilePath)),
-    ActionOnSubscribeAction = id => Task.Run(() => Console.WriteLine("FileWatcher subscribe action complete for file {0}", id.FilePath)),
-    ActionOnSubscribeActionFailed = ex => Task.Run(() => Console.WriteLine("FileWatcher subscribe failure for file {0}", ex.FileIdentifier.FilePath))
+    ActionOnFileReloaded = async id => await Task.Run(() => Console.WriteLine("FileWatcher reload complete for file {0}", id.FilePath)),
+    ActionOnFileReloadFailed = async ex => await Task.Run(() => Console.WriteLine("FileWatcher reload failure for file {0}", ex.FileIdentifier.FilePath)),
+    ActionOnSubscribeAction = async id => await Task.Run(() => Console.WriteLine("FileWatcher subscribe action complete for file {0}", id.FilePath)),
+    ActionOnSubscribeActionFailed = async ex => await Task.Run(() => Console.WriteLine("FileWatcher subscribe failure for file {0}", ex.FileIdentifier.FilePath))
 };
 var fileWatcher = new CyclicalFileWatcher<FileObject>(configuration);
 var parameters = new FileWatcherParameters<FileObject>
@@ -27,10 +27,10 @@ var parameters = new FileWatcherParameters<FileObject>
     FilePath = filePath,
     Depth = depth,
     FileStateContentFactory = async path => new FileObject {Content = await File.ReadAllTextAsync(path, cts.Token) },
-    FileStateKeyFactory = (_, _) => Task.FromResult(Guid.NewGuid().ToString())
+    FileStateKeyFactory = async (_, _) => await Task.FromResult(Guid.NewGuid().ToString())
 };
 await fileWatcher.WatchAsync(parameters, cts.Token);
-await fileWatcher.SubscribeAsync(filePath,  _ => Task.Run(() => Console.WriteLine("Executing subscription action")), cts.Token);
+await fileWatcher.SubscribeAsync(filePath,  async _ => await Task.Run(() => Console.WriteLine("Executing subscription action")), cts.Token);
 
 Console.WriteLine($"Started testing for filepath {filePath}");
 
@@ -44,5 +44,7 @@ try
 }
 catch (TaskCanceledException)
 {
+    // Expected to be canceled.
+    Console.WriteLine($"Testing for filepath {filePath} cancelled, finishing test.");
 }
 
